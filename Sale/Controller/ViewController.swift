c1da13e2ee7179de9ea2860f1a086d8385163186
Iurie Guzun <iurie_guzun@yahoo.com>
2021-10-23 11:51:22 -0400

    Initial Commit

---
+++ Sale/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sale
+//
+//  Created by Iurie Guzun on 2021-10-23.
+//  Copyright © 2021 Iurie Guzun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Sale/Model/Product.swift
@@ -0,0 +1,9 @@
+//
+//  Product.swift
+//  Sale
+//
+//  Created by Iurie Guzun on 2021-10-23.
+//  Copyright © 2021 Iurie Guzun. All rights reserved.
+//
+
+import Foundation
