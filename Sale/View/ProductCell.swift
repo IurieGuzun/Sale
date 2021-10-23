857069629b6c346ba57516b8afe4723f62b73a1d
Iurie Guzun <iurie_guzun@yahoo.com>
2021-10-23 12:01:23 -0400

    Added Product Cell File

---
+++ Sale/View/ProductCell.swift
@@ -0,0 +1,20 @@
+//
+//  ProductCell.swift
+//  Sale
+//
+//  Created by Iurie Guzun on 2021-10-23.
+//  Copyright © 2021 Iurie Guzun. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    
+    func configure(with product: Product) {
+        titleLabel.text = product.title
+        priceLabel.text = product.price()
+    }
+    }
